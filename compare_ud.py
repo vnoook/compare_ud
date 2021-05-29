7e3c7ebc902d301fc49c38b3d9aad62aa9b1f348
vnoook <noook@yandex.ru>
2021-05-29 20:37:06 +0700

    #13

--- compare_ud.py
+++ compare_ud.py
@@ -2,12 +2,12 @@
 import openpyxl
 import openpyxl.styles as ops
 
-file_xls = 'sravnenie ud.xlsx'
+file_xls = 'compare_ud.xlsx'
 start_row = 2
 data_col1 = 1
 data_col2 = 2
-sp1 = set()
-sp2 = set()
+set1 = set()
+set2 = set()
 
 wb = openpyxl.load_workbook(file_xls)
 ws = wb.active
@@ -19,7 +19,7 @@ for row in range(start_row, max_row+1):
     if orig_content_cell:
         content_cell = str(orig_content_cell.strip()).replace('.', '')
         if content_cell.isdigit():
-            sp1.add(content_cell)
+            set1.add(content_cell)
         else:
             cell.fill = ops.PatternFill(start_color='FF0000', end_color='FF0000', fill_type='solid')
     else:
@@ -31,18 +31,18 @@ for row in range(start_row, max_row+1):
     if orig_content_cell:
         content_cell = str(orig_content_cell.strip()).replace('.', '')
         if content_cell.isdigit():
-            sp2.add(content_cell)
+            set2.add(content_cell)
         else:
             cell.fill = ops.PatternFill(start_color='FF0000', end_color='FF0000', fill_type='solid')
     else:
         cell.fill = ops.PatternFill(start_color='000000', end_color='FF0000', fill_type='solid')
 
-dict1 = enumerate(sp1)
+dict1 = enumerate(set1)
 for i, data in dict1:
     print(i, data)
     ws.cell(start_row+i, data_col1+2).value = data
 
-dict2 = enumerate(sp2)
+dict2 = enumerate(set2)
 for i, data in dict1:
     print(i, data)
     ws.cell(start_row+i, data_col2+2).value = data
