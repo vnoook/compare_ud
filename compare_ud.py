f3c8320fdce24a4d9cc017291508a472dbcdf6a6
vnoook <noook@yandex.ru>
2021-05-29 21:34:57 +0700

    #17 заполение ячеек

--- compare_ud.py
+++ compare_ud.py
@@ -2,16 +2,23 @@
 import openpyxl
 import openpyxl.styles
 
+# переменные для работы
 file_xls = 'compare_ud.xlsx'
+# строка с которой начинать собирать данные
 start_row = 2
+# колонки в которые пользователь вставляет данные
 data_col1 = 1
 data_col2 = 2
+# два множества для данных из колонок со вставленными данными
 set1 = set()
 set2 = set()
 
+# открывается существующая книга эксель
 wb = openpyxl.load_workbook(file_xls)
 ws = wb.active
+# переменная самой последней заполненной строки
 max_row = ws.max_row
+
 
 for row in range(start_row, max_row+1):
     cell = ws.cell(row, data_col1)
@@ -37,15 +44,15 @@ for row in range(start_row, max_row+1):
     else:
         cell.fill = openpyxl.styles.PatternFill(start_color='000000', end_color='FF0000', fill_type='solid')
 
-dict1 = enumerate(set1)
-for i, data in dict1:
-    print(i, data)
-    ws.cell(start_row+i, data_col1+2).value = data
 
-dict2 = enumerate(set2)
-for i, data in dict1:
-    print(i, data)
-    ws.cell(start_row+i, data_col2+2).value = data
+# заполнение колонок в экселе
+tuple1 = enumerate(set1)
+for i1, data1 in tuple1:
+    ws.cell(start_row+i1, data_col1+2).value = data1
 
+
+tuple2 = enumerate(set2)
+for i2, data2 in tuple2:
+    ws.cell(start_row+i2, data_col2+2).value = data2
 
 wb.save(file_xls)
