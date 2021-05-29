5b2d44e5a6e904ec1be360299b6d18c9bc5e1239
vnoook <noook@yandex.ru>
2021-05-29 22:31:30 +0700

    #19 заполение ячеек

--- compare_ud.py
+++ compare_ud.py
@@ -46,20 +46,39 @@ for row in range(start_row, max_row+1):
 
 
 # заполнение колонок в экселе
+# чистка первой колонки - без дублей и пустых ячеек
 tuple1 = enumerate(set1)
 for i1, data1 in tuple1:
-    ws.cell(start_row+i1, data_col1+2).value = data1
+    ws.cell(start_row+i1, 3).value = data1
 
+# чистка второй колонки - без дублей и пустых ячеек
 tuple2 = enumerate(set2)
 for i2, data2 in tuple2:
-    ws.cell(start_row+i2, data_col2+2).value = data2
+    ws.cell(start_row+i2, 4).value = data2
 
+# Объединение списков без дублей
+all_cols = set1.union(set2)
+tuple3 = enumerate(all_cols)
+for i3, data3 in tuple3:
+    ws.cell(start_row+i3, 5).value = data3
 
+# общие значения
+common_values = set1.intersection(set2)
+tuple4 = enumerate(common_values)
+for i4, data4 in tuple4:
+    ws.cell(start_row+i4, 6).value = data4
 
+# пересечение 1
+diff_set1 = set1.difference(set2)
+tuple5 = enumerate(diff_set1)
+for i5, data5 in tuple5:
+    ws.cell(start_row+i5, 7).value = data5
 
-
-
-
+# пересечение 2
+diff_set2 = set2.difference(set1)
+tuple6 = enumerate(diff_set1)
+for i6, data6 in tuple6:
+    ws.cell(start_row+i6, 8).value = data6
 
 
 wb.save(file_xls)
